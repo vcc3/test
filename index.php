be941c80405cac1a0c9ec13e05451f5dd8f2f2b0
vcc3 <vcc3@njit.edu>
2017-02-24 14:34:32 -0500

    adding all files from other repository  to run on heroku

---
+++ index.php
@@ -0,0 +1,357 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Cornejo</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <!-- Theme CSS -->
+    <link href="css/grayscale.min.css" rel="stylesheet">
+    <link href ="css/grayscale.css" rel ="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
+                    Menu <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand page-scroll" href="#page-top">
+                </a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
+                <ul class="nav navbar-nav">
+                    <!-- Hidden li included to remove active class from about link when scrolled up past about section -->
+                    <li class="hidden">
+                        <a href="#page-top"></a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#about">About</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#education">Education</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#work">Work</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#skills">Skills</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#contact">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <!-- Intro Header -->
+    <header class="intro">
+        <div class="intro-body">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 col-md-offset-2 col-sm-8 col-sm-offset-2">
+                        <h1 class="brand-heading">Cornejo</h1>
+                        <p class="intro-text">Resume Web Page for 
+                            <br>Victor Carlos Cornejo.</p>
+                        <a href="#about" class="btn btn-circle page-scroll">
+                            <i class="fa fa-angle-double-down animated"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <!-- About Section -->
+     <section id="about" class="content-section text-center">
+        <div class="download-section">
+            <div class="container">
+                <div class="col-lg-8 col-lg-offset-2 col-sm-8 col-sm-offset-2">
+                     <h1>About Me</h1>
+                <br>
+                  
+                      <p> </p>
+                    
+                    
+                    
+                </div>
+            </div>
+        </div>
+    </section>
+
+     <!-- Education Section -------------------------------------------------------------------------------------------->
+    <section id="education" class="content-section text-center">
+        <div class="download-section">
+            <div class="container">
+                <div class="col-lg-8 col-lg-offset-2 col-sm-8 col-sm-offset-2">
+                    <h1>Education</h1>
+                     <hr> 
+                   <div>
+                      <img src ="img/wallkill.jpg" width = 120px height = 120px>
+                      <p><b>Wallkill Valley Regional High School</b></p>
+                      <p>Graduated: June 2012</p>
+                      <p>10 Grumm Rd, Hamburg, NJ 07419</p>
+                   </div>                                                
+                   <hr>
+                   <div>
+                    <img src ="img/sussex.jpg" width = 120px height = 120px>
+                     <p><b>Sussex County Community College </b></p>
+                     <p>Graduated: May 2015 -AS in Computer Science </p>
+                     <p>1 College Hill Rd, Newton, NJ 07860</p> 
+                   </div>
+                   <hr>               
+                   <div>
+                     <img src ="img/njit.jpg" width = 120px height = 120px>
+                     <p><b>New Jersey Institute of Technology</b></p>
+                     <p> *Currently Attending*-Expected Grad Date: May 2018</p>
+                     <p>323 Dr Martin Luther King Jr Blvd, Newark, NJ 07102</p>
+                   </div> 
+                   <hr> 
+                    
+                    
+                    
+                </div>
+            </div>
+        </div>
+    </section>
+     <!-- Work Section --------------------------------------------------------------------------------->
+    <section id="work" class="content-section text-center">
+        <div class="download-section">
+            <div class="container">
+                <div class="col-lg-8 col-lg-offset-2 col-sm-8 col-sm-offset-2">
+                    <h1>Work  Experience</h1>
+                  <p>PlanIT Networks, Blairstown NJ(July 2015-July 2016)</p>
+                    <ul>
+                      <li>Telecommunications/ Cabling /Server Room Patching.</li>
+                      <li>On site, on call IT technician for companies without one.</li>
+                      <li>Installing equipment racks, termination of cables, running of cables wiring for systems.Cat5 /5e /Cat 6</li>
+                      <li>Punch down of cables to Male/Female ends or Panel.</li>
+                    </ul>  
+                  <p>Delaware Cemetery Association, Dingmans Ferry ,PA(Summer, 2014)</p>
+                    <ul>
+                      <li>	Created a website structure for a Cemetery association.</li>
+                      <li>Met with client to discuss goals and deadlines.</li>
+                      <li>Delivered final submission that met client&#39;s expectations.</li>
+                      <li>Website - http://delawarecemeterypa.com, Internship opportunity.</li>
+                    </ul> 
+                  <p>Weis Markets Newton ,NJ (August 2012- August 2015</p>
+                    <ul>
+                      <li>Produce Clerk,  building and stocking displays.</li>
+                      <li>Working night shifts, or opening department</li>
+                      <li>Unloading of delivery to coolers /freezer.</li>
+                    </ul>   
+                 <p>The Pet Spa and Resort, Franklin NJ(August 2016-Current)</p>
+                    <ul>
+                      <li>Walk,feed, take care of customers pets.</li>
+                      <li>Constant cleaning to make sure things are steralized and germ free.</li>
+                    </ul>                        
+                    
+                    
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Skills Section ------------------------------------------------------------------------------->
+    <section id="skills" class="content-section text-center">
+        <div class="download-section">
+            <div class="container">
+                <div class="col-lg-8 col-lg-offset-2 col-sm-8 col-sm-offset-2">
+                         <h1>Skills & Abilities</h1>
+                 <p>Programming languages worked in:</p>
+                   <ul>
+                     <li>C++</li>
+                     <li>Java</li>
+                     <li>Javascript</li>
+                     <li>HTML/CSS</li>
+                     <li>PHP</li>
+                     <li>Python</li>
+                   </ul>
+                   <p>Bilingual: English/Spanish</p>
+                   <p>Usage of Git, and GitHub and Trello </p>
+                   <p>Able to adapt in different working environments.</p>
+                   <p> Communication with others to clearly depict what is needed and must be done for a project to be completed.<p>
+                    
+                    
+                    
+                </div>
+            </div>
+        </div>
+    </section>
+   <!-- Contact Section ------------------------------------------------------------------------------>
+     <section id="contact" class="content-section text-center">
+        <div class="download-section">
+            <div class="container">
+                <div class="col-lg-8 col-lg-offset-2 col-sm-8 col-sm-offset-2">
+                         
+                       <h2>Contact Victor Carlos</h2>
+                
+                <br>
+                <br>
+                <p> The button below will redirect to my Github page.</p>
+                <ul class="list-inline banner-social-buttons">
+                   
+                    <li>
+                        <a href="https://github.com/vcc3" class="btn btn-default btn-lg"><i class="fa fa-github fa-fw"></i> <span class="network-name">Github</span></a>
+                    </li>
+                </ul>
+                    <p> Need to contact me? Fill out the form below to send me an email! Phone number is optional.</p>
+                     <form name="contactform" method="post" action="send_form_email.php">
+ 
+                          <table width="450px">
+                   
+                            <tr>
+                   
+                             <td valign="top">
+                   
+                              <label for="first_name">First Name *</label>
+                   
+                             </td>
+                   
+                             <td valign="top">
+                   
+                              <input  type="text" name="first_name" maxlength="50" size="30">
+                   
+                             </td>
+                   
+                            </tr>
+                   
+                            <tr>
+                   
+                             <td valign="top"">
+                   
+                              <label for="last_name">Last Name *</label>
+                   
+                             </td>
+                   
+                             <td valign="top">
+                   
+                              <input  type="text" name="last_name" maxlength="50" size="30">
+                   
+                             </td>
+                   
+                            </tr>
+                   
+                            <tr>
+                   
+                             <td valign="top">
+                   
+                              <label for="email">Email Address *</label>
+                   
+                             </td>
+                   
+                             <td valign="top">
+                   
+                              <input  type="text" name="email" maxlength="80" size="30">
+                   
+                             </td>
+                   
+                            </tr>
+                   
+                            <tr>
+                   
+                             <td valign="top">
+                   
+                              <label for="telephone">Telephone Number</label>
+                   
+                             </td>
+                   
+                             <td valign="top">
+                   
+                              <input  type="text" name="telephone" maxlength="30" size="30">
+                   
+                             </td>
+                   
+                            </tr>
+                   
+                            <tr>
+                   
+                             <td valign="top">
+                   
+                              <label for="comments">Subject Body *</label>
+                   
+                             </td>
+                   
+                             <td valign="top">
+                   
+                              <textarea  name="comments" maxlength="1000" cols="25" rows="6"></textarea>
+                   
+                             </td>
+                   
+                            </tr>
+                   
+                            <tr>
+                   
+                             <td colspan="2" style="text-align:center">
+                   
+                             <input type="submit" value="Submit">   
+                   
+                            </td>
+                   
+                          </tr>
+                   
+                        </table>
+ 
+                </form>   
+                         
+                         
+                    
+                    
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Footer -->
+    <footer>
+        <div class="container text-center">
+            <p>Copyright &copy; My Website for School 2017</p>
+        </div>
+    </footer>
+
+    <!-- jQuery -->
+    <script src="vendor/jquery/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+
+   
+
+    <!-- Theme JavaScript -->
+    <script src="js/grayscale.min.js"></script>
+
+</body>
+
+</html>
