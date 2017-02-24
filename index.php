acb99d6a32ad565fbcd977794a8f6ffa20f35b9b
vcc3 <vcc3@njit.edu>
2017-02-24 15:50:21 -0500

    removed form for sending

--- index.php
+++ index.php
@@ -10,7 +10,11 @@
     <meta name="author" content="">
 
     <title>Cornejo</title>
+ <script type="text/javascript" src="http://ajax.googleapis.com/
 
+  ?ajax/libs/jquery/1.5.1/jquery.min.js"></script>
+
+  <script type='text/javascript' src='script.js'></script>
     <!-- Bootstrap Core CSS -->
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
@@ -227,105 +231,7 @@
                         <a href="https://github.com/vcc3" class="btn btn-default btn-lg"><i class="fa fa-github fa-fw"></i> <span class="network-name">Github</span></a>
                     </li>
                 </ul>
-                    <p> Need to contact me? Fill out the form below to send me an email! Phone number is optional.</p>
-                     <form name="contactform" method="post" action="send_form_email.php">
- 
-                          <table width="400px">
-                   
-                            <tr>
-                   
-                             <td valign="top">
-                   
-                              <label for="first_name">First Name *</label>
-                   
-                             </td>
-                   
-                             <td valign="top">
-                   
-                              <input  type="text" name="first_name" maxlength="50" size="30">
-                   
-                             </td>
-                   
-                            </tr>
-                   
-                            <tr>
-                   
-                             <td valign="top"">
-                   
-                              <label for="last_name">Last Name *</label>
-                   
-                             </td>
-                   
-                             <td valign="top">
-                   
-                              <input  type="text" name="last_name" maxlength="50" size="30">
-                   
-                             </td>
-                   
-                            </tr>
-                   
-                            <tr>
-                   
-                             <td valign="top">
-                   
-                              <label for="email">Email Address *</label>
-                   
-                             </td>
-                   
-                             <td valign="top">
-                   
-                              <input  type="text" name="email" maxlength="80" size="30">
-                   
-                             </td>
-                   
-                            </tr>
-                   
-                            <tr>
-                   
-                             <td valign="top">
-                   
-                              <label for="telephone">Telephone Number</label>
-                   
-                             </td>
-                   
-                             <td valign="top">
-                   
-                              <input  type="text" name="telephone" maxlength="30" size="30">
-                   
-                             </td>
-                   
-                            </tr>
-                   
-                            <tr>
-                   
-                             <td valign="top">
-                   
-                              <label for="comments">Subject Body *</label>
-                   
-                             </td>
-                   
-                             <td valign="top">
-                   
-                              <textarea  name="comments" maxlength="1000" cols="25" rows="6"></textarea>
-                   
-                             </td>
-                   
-                            </tr>
-                   
-                            <tr>
-                   
-                             <td colspan="2" style="text-align:center">
-                   
-                             <input type="submit" value="Submit">   
-                   
-                            </td>
-                   
-                          </tr>
-                   
-                        </table>
- 
-                </form>   
-                         
+                  
                          
                     
                     
